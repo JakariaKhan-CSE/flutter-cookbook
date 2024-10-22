e599eca09fb7f99dd68367ea78f278b7842a2b1e
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2024-10-22 14:55:26 +0600

    download button effect create complete.

--- lib/Effects/downloadButton/create_download_button.dart
+++ lib/Effects/downloadButton/create_download_button.dart
@@ -1,5 +1,10 @@
+import 'package:cookbook_flutter/Effects/downloadButton/SimulatedDownloadController.dart';
+import 'package:cookbook_flutter/Effects/downloadButton/demo_app_icon.dart';
+import 'package:cookbook_flutter/Effects/downloadButton/download_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+
+import 'download_controller.dart';
 //In Dart and Flutter, immutability refers to the concept of creating
 // objects whose state cannot be modified after creation
 @immutable
@@ -11,12 +16,16 @@ class CreateDownloadButton extends StatefulWidget {
 }
 
 class _CreateDownloadButtonState extends State<CreateDownloadButton> {
-   // late final List<DownloadController> _downloadController;
+   late final List<DownloadController> _downloadControllers;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    // write required code
+    // 20 ta list auto generate hobe
+    _downloadControllers = List<DownloadController>.generate(20,
+            (index)=> SimulatedDownloadController(onOpenDownload:(){
+      _openDownload(index);
+    }));
 
   }
   void _openDownload(int index){
@@ -27,15 +36,35 @@ class _CreateDownloadButtonState extends State<CreateDownloadButton> {
   Widget build(BuildContext context) {
     return Scaffold(
     appBar:  AppBar(title: Text('Download Effect Demo'),centerTitle: true,),
-      // body: ListView.separated(
-      //     itemBuilder: itemBuilder,
-      //     separatorBuilder: separatorBuilder,
-      //     itemCount: itemCount),
+      body: ListView.separated(
+        itemCount: _downloadControllers.length,
+          itemBuilder: _buildListItem,
+          separatorBuilder: (context, index) => const Divider(),
+          ),
     );
   }
   Widget _buildListItem(BuildContext context, int index){
     final theme = Theme.of(context);
-    return ListTile();
+    final downloadController = _downloadControllers[index];
+    return ListTile(
+      leading: DemoAppIcon(),
+      title: Text('App ${index+1}',overflow: TextOverflow.ellipsis,
+      style: theme.textTheme.bodySmall,),
+      trailing: SizedBox(
+        width: 96,
+        child: AnimatedBuilder(
+            animation: downloadController,
+            builder: (context, child) {
+              return DownloadButton(
+                  status: downloadController.downloadStatus,
+                  downloadProgress: downloadController.progress,
+                  onDownload: downloadController.startDownload,
+                  onCancel: downloadController.stopDownload,
+                  onOpen: downloadController.openDownload);
+            },
+        ),
+      ),
+    );
   }
 }
 
